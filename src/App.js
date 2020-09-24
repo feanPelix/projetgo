e59be66ad97236dcba5abf3f782c415f7c32e45f
Lauriane <lduchesneauduruisseau@gmail.com>
2020-09-24 14:21:12 -0400

    Work on router

--- src/App.js
+++ src/App.js
@@ -3,25 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+import Switch from 'react-bootstrap/esm/Switch';
+import ProjectCard from './components/ProjectCard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="warning">Primary</Button>
       </header>
+      <div className="App-main">
+        {/* <Router>
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+          </Switch>
+
+        </Router> */}
+        <ProjectCard />
+      </div>
+      <footer className="App-footer">
+
+      </footer>
     </div>
   );
 }
