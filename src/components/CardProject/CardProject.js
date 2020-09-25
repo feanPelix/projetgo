e5e75c93b20199430c4ab5055a71824f8200307d
Lauriane <lduchesneauduruisseau@gmail.com>
2020-09-24 20:46:31 -0400

    Button donation

--- src/components/CardProject/CardProject.js
+++ src/components/CardProject/CardProject.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 
-import ButtonDonation from './ButtonDonation';
+import ButtonDonation from '../ButtonDonation/ButtonDonation';
 
-export function ProjectCard() {
+export function ProjectCard({srcImage, name, summary}) {
+  const variant = null;
+
   return (
     <Card 
       style={{ width: '18rem' }}
     >
       <Card.Img 
-        variant="top" 
-        src={}
+        variant={variant}
+        src={srcImage}
       />
       <Card.Body>
         <Card.Title>
-          {}
+          {name}
         </Card.Title>
         <Card.Text>
-          {}
+          {summary}
         </Card.Text>
           <ButtonDonation />
       </Card.Body>
