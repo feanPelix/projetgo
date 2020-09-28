111081a190c2424cff6873da11b152df68b2140c
Lauriane <lduchesneauduruisseau@gmail.com>
2020-09-28 13:52:33 -0400

    Reorganize Cardproject files

---
+++ src/components/CardProject/CardProject.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import './CardProject.css';
+import ButtonDonation from '../ButtonDonation/ButtonDonation';
+
+
+
+export function ProjectCard({srcImage, name, summary}) {
+  const size = "small";
+  let styles = `bkg ${size}`;
+
+  return (
+    <Card 
+      className={styles}
+    >
+      <Card.Img 
+        variant="top"
+        src={srcImage}
+      />
+      <Card.Body>
+        <Card.Title>
+          {name}
+        </Card.Title>
+        <Card.Text>
+          {summary}
+        </Card.Text>
+          <ButtonDonation />
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ProjectCard;
