5aaf365d6e5b0b91f488c450a5f309e2cdb24e1b
jf <6155169@crosemont.qc.ca>
2020-09-28 17:46:53 -0400

    update README.md

--- backend/server/index.js
+++ backend/server/index.js
@@ -9,10 +9,10 @@ app.use(cors());
 app.use(express.json());
 
 // serve static files built by React
-app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(express.static(path.join(__dirname, "../../frontend/build")));
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/build", "index.html"));
 });
 
 //ROUTES//
--- frontend/README.md
+++ frontend/README.md
@@ -2,6 +2,7 @@
 1. L'application React doit etre 'build'. Donc à partir de `projetgo/frontend`:
     `npm run-script build`
     
-2. Dans le fichier `projetgo/backend` lancez le serveur:
-    `node server`
+2. Dans le fichier `projetgo/backend/server` lancez le serveur:
+    `node index`
     
+3. L'url pour accéder au site est `localhost:5000`
