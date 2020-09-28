0a4df1d5f2c0679eea9ea9ef138b6460dd272cea
Lauriane <lduchesneauduruisseau@gmail.com>
2020-09-28 18:35:09 -0400

    Home container with video and project cards

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactPlayer from 'react-player/youtube';
+import './VideoPlayer.css';
 
 export function VideoPlayer() {
   return (
     <ReactPlayer
+      className="vd-center"
       url="https://www.youtube.com/watch?v=7sDY4m8KNLc"
       width="64rem"
       height="36rem"
