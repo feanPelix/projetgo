1e12279a91618da154ec65e978fc9c4da6e8ea31
Lauriane <lduchesneauduruisseau@gmail.com>
2020-09-29 16:59:48 -0400

    Footer funding highligth section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,55 @@
 import React from 'react';
-import { Container, Col, Row } from 'react-bootstrap';
+import { Container, Col, Row, ListGroup } from 'react-bootstrap';
+import { Facebook, Twitter, Instagram } from 'react-feather';
 
 export function Footer() {
+  const [projects, setProjects] = useState([]);
+
+  const getProjects = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/Accueil");
+      const jsonData = await response.json();
+
+      setProjects(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
+  const highligthProjects = projects.map(key => {
+    return (
+      <ListGroup.Item
+        key={key}
+      >
+        {key.title}
+
+      </ListGroup.Item>
+    );
+  });
+
+
   return(
     <Container fluid>
       <Row>
         <Col lg={4}>
           <h4>Campagnes</h4>
+          <ListGroup variant="flush">
+            {highligthProjects}
+          </ListGroup>
         </Col>
         <Col lg={4}>
           <h4>Mon compte</h4>
         </Col>
         <Col lg={4}>
           <h4>Restez en contact!</h4>
-
+          <Facebook />
+          <Twitter />
+          <Instagram />
+          <p>Contactez-nous</p>
         </Col>
       </Row>
       <Row>
