a1eefb563fb36e241a90a8feecba8aed09d5fabc
jf <6155169@crosemont.qc.ca>
2020-10-18 19:42:03 -0400

    Centrer les cardProject1

--- frontend/src/components/Login/ListerProjects.js
+++ frontend/src/components/Login/ListerProjects.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Col, Row, Container, Image } from "react-bootstrap";
+import {Col, Row, Container, Image, CardDeck} from "react-bootstrap";
+import CardProject from '../CardProject/CardProject';
 import ButtonPG from '../Buttons/ButtonPG/ButtonPG';
 
 function ListerProjects({ match, history, loggedInMemberID }) {
@@ -25,34 +26,24 @@ function ListerProjects({ match, history, loggedInMemberID }) {
   const [listProjects, setListProjects] = useState([]);
   // get the specific project number and push to the detail
 
-
-
   return (
-
     <Container>
+      <CardDeck>
         {listProjects.map(project =>
-          <div key={project.code}>
-            <Row className="shadow p-3 mb-5 bg-white rounded p-4">
-              <Col className="mr-4" lg={5} sm={12}>
-                <Image fluid src={project.image} />
-              </Col><br /><br />
-              <Col className="ml-4" lg={6} sm={12}>
-                <Row>
-                  <div><h2>{project.titre}</h2></div>
-                </Row><br />
-                <Row>
-                  <div style={{ textAlign: 'left', fontSize: '18px' }}>{project.description}</div>
-                </Row><br />
-                <Row>
-                  <ButtonPG onClick={() => history.push(`${match.url}/${project.code}`)}>
-                    Details
-                  </ButtonPG>
-                </Row>
-              </Col>
-            </Row><br />
-          </div>)}
+          <Col
+            lg={4}
+            md={4}
+            key={project.code}
+          >
+            <CardProject
+              srcImage={project.image}
+              title={project.titre}
+              summary={project.description}
+            />
+          </Col>
+        )}
+      </CardDeck>
     </Container>
-
   )
 }
 
