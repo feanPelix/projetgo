b858b4c891293d2cfa72256c931a3c6ba49a4428
LaurianeDD <lduchesneauduruisseau@gmail.com>
2020-10-21 16:36:40 -0400

    finalEditMarina (#33)
    Co-authored-by: marinafl <marina.lzfz@gmail.com>

--- frontend/src/components/Welcome/Welcome.js
+++ frontend/src/components/Welcome/Welcome.js
@@ -1,23 +1,24 @@
 import React, { useContext } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Col, Row, Image, Badge } from "react-bootstrap";
+import { Button,Container, Col, Row, Image, Badge } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import { AuthContext } from '../context/AuthContext/AuthContext';
 import ButtonPG from '../Buttons/ButtonPG/ButtonPG';
+import './Welcome.css';
 
 function Welcome() {
   const { state: { user, member } } = useContext(AuthContext);
   const history = useHistory();
 
   return (
-    <Container className="shadow p-5 mb-5 bg-white rounded p-4">
+    <Container style={{textAlign:"center"}} className="shadow p-5 mb-5 bg-white rounded p-4">
       <Row >
         <Col xs={12} md={6} lg={5} >
           <Image style={{ maxWidth: "70%" }} fluid src='/images/avatar_woman.png' />
         </Col>
 
-        <Col className="p-4" xs={12} md={6} lg={7}>
-          <h1 style={{ letterSpacing: 3 }}>Bon retour</h1>
+        <Col className="p-4" xs={12} md={6} lg={6}>
+          <h1 style={{ letterSpacing: 3}}>Bon retour</h1>
           <h1 style={{ letterSpacing: 2 }}>{user.nom + " " + user.prenom} !</h1>
           <Badge pill variant="warning" className="mt-4 p-3 px-5" >
             {
@@ -25,40 +26,29 @@ function Welcome() {
               `Membre: ${member.statutadhesion}` :
               'Bénévole'
             }
-          </Badge>
+          </Badge><br/><br/><br/>
+          <Row>
+            <Col>
+              <div  className="btn-group" role="group" >
+                <Button variant="orange" className="btn-welcome pl-4" type="submit" size="lg" onClick={() => history.push('/membre/profil')}>
+                  Profil
+                </Button>
+                <Button  variant="orange" className="btn-welcome" type="submit" size="lg" onClick={() => history.push('/membre/mesProjets')}>
+                  Mes projets
+                </Button>{' '}
+                {!!member && (
+
+                    <Button  variant="orange" className="btn-welcome pr-4" type="submit" size="lg" onClick={() => history.push('/membre/mesProjets/nouveau')}>
+                      Créer un projet
+                    </Button>
+                )}
+              </div>
+            </Col>
+          </Row>
+
         </Col>
       </Row>
-      <Row>
-        <Col>
-          <ButtonPG
-            type="submit"
-            size="lg"
-            onClick={() => history.push('/membre/profil')}
-          >
-            Profil
-          </ButtonPG>
-        </Col>
-        <Col>
-          <ButtonPG
-            type="submit"
-            size="lg"
-            onClick={() => history.push('/membre/mesProjets')}
-          >
-            Mes projets
-          </ButtonPG>
-        </Col>
-        {!!member && (
-          <Col>
-            <ButtonPG
-              type="submit"
-              size="lg"
-              onClick={() => history.push('/membre/mesProjets/nouveau')}
-            >
-              Créer un projet
-            </ButtonPG>
-          </Col>
-        )}
-      </Row>
+
     </Container>
 
   );
