12ab0ec95edd49824b0804902a580b3e19530fd3
jf <6155169@crosemont.qc.ca>
2020-10-28 13:14:17 -0400

    reset db config

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,15 +1,10 @@
 const { Pool } = require('pg');
 require('dotenv/config');
 
-const poolConfig = process.env.DB_CONNECTION ? {
-    connectionString: process.env.DB_CONNECTION,
-} : {
-    user: "postgres",
-    password: "doorpaste",
-    host: "35.188.252.102",
-    port: "5432",
-    database: "projetgodb"
-};
-const pool = new Pool(poolConfig);
+const pool = new Pool({
+  connectionString: process.env.DB_CONNECTION,
+});
 
-module.exports = pool; 
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+}
